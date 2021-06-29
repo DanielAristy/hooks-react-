c1d70cc967facb671261a12462a4edb27cd7ec4d
DanielAristy <danielaristy22@gmail.com>
2021-06-28 21:12:39 -0500

    Utilizando una aplicacion en react para una peticion GET y utilizando hooks de react useState y useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
+const API = 'https://jsonplaceholder.typicode.com/users';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  /*
+  const users = [
+    {name: "Daniel Aristizabal", username: "DanielAristy", email:"danielaristy@gmail.com", telefono: 5484840},
+    {name: "Jose Pereza", username: "JosePerez", email:"danielaristy@gmail.com", telefono: 5484840},
+  ];*/
+
+  //Api
+  // const [users, setUsers] = useState([
+  //   {name: "Daniel Aristizabal", username: "DanielAristy", email:"danielaristy@gmail.com", telefono: 5484840},
+  //   {name: "Jose Pereza", username: "JosePerez", email:"danielaristy@gmail.com", telefono: 5484840},
+  // ])
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => setUsers(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  //Aumentar numero
+  const [contador, setContador] = useState(0)
+
+  return (<Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Compañia</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.company.name}</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Contador</h2>
+      <p>La cuenta va en {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Sumar</button>
+      <button onClick={() => setContador(contador - 1)}>Restar</button>
+    </Fragment>
   );
 }
 
