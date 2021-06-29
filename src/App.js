161f7e3a10221cf19ee20ece3fd14d75872e58b5
DanielAristy <danielaristy22@gmail.com>
2021-06-28 22:09:09 -0500

    Dandole un poco de estilo a la pagina con Bootstrap

--- src/App.js
+++ src/App.js
@@ -29,32 +29,36 @@ function App() {
   const [contador, setContador] = useState(0)
 
   return (<Fragment>
-      <table>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>UserName</th>
-            <th>Email</th>
-            <th>Teléfono</th>
-            <th>Compañia</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.username}</td>
-            <td>{user.email}</td>
-            <td>{user.phone}</td>
-            <td>{user.company.name}</td>
-          </tr>
-          ))}
-        </tbody>
-      </table>
-      <h2>Contador</h2>
-      <p>La cuenta va en {contador}</p>
-      <button onClick={() => setContador(contador + 1)}>Sumar</button>
-      <button onClick={() => setContador(contador - 1)}>Restar</button>
+      <div className="container">
+        <h2>Lista de Usuarios</h2>
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr className="table-dark">
+              <th scope="col">Nombre</th>
+              <th scope="col">UserName</th>
+              <th scope="col">Email</th>
+              <th scope="col">Teléfono</th>
+              <th scope="col">Compañia</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.company.name}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+        <h2>Contador</h2>
+        <p>La cuenta va en {contador}</p>
+        <button onClick={() => setContador(contador + 1)}>Sumar</button>
+        <button onClick={() => setContador(contador - 1)}>Restar</button>
+      </div>
+      
     </Fragment>
   );
 }
